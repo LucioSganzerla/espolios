acf18eb55ed56ab242ea235218d42ae7e9f5fd6b
LucioSganzerla <sganzerla.lucio1@gmail.com>
2022-05-06 18:50:24 -0300

    Initial commit

---
+++ src/main/java/br/edu/utfpr/espolios/EspoliosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.espolios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspoliosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EspoliosApplication.class, args);
+    }
+
+}
