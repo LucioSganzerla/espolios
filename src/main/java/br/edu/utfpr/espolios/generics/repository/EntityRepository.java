c75fca2723d4ff2bb6c782c934716dbfb9e3311a
LucioSganzerla <sganzerla.lucio1@gmail.com>
2022-05-20 20:21:52 -0300

    Abrir bau service

--- src/main/java/br/edu/utfpr/espolios/generics/repository/EntityRepository.java
+++ src/main/java/br/edu/utfpr/espolios/generics/repository/EntityRepository.java
@@ -5,9 +5,12 @@ import br.edu.utfpr.espolios.models.enums.Rarity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import java.util.List;
+
 @NoRepositoryBean
 public interface EntityRepository<T extends EntityModel> extends JpaRepository<T, Long> {
 
     T findByRarity(Rarity rarity);
+    List<T> findAllByRarity(Rarity rarity);
 
 }
---
+++ src/main/java/br/edu/utfpr/espolios/service/AbrirBauService.java
@@ -0,0 +1,53 @@
+package br.edu.utfpr.espolios.service;
+
+import br.edu.utfpr.espolios.models.enums.Rarity;
+import br.edu.utfpr.espolios.service.CRUD.BauService;
+import br.edu.utfpr.espolios.service.CRUD.ChaveService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public record AbrirBauService(BauService bauService,
+                              ChaveService chaveService) {
+
+    private boolean validaQuantidadeChaves(Rarity rarity, Integer quantidade) {
+        log.info("Validando quantidade de chaves, para verificar se é possivel abrir o bau");
+        boolean resut = chaveService.repository().findByRarity(rarity).getQuantidade() >= (quantidade);
+        if (resut) {
+            log.info("É possível abrir " + quantidade + " baus do tipo " + rarity);
+        } else {
+            log.info("Não é possível abrir " + quantidade + " baus do tipo " + rarity);
+        }
+        return resut;
+    }
+
+    private boolean validaQuantidadeBaus(Rarity rarity, Integer quantidade) {
+        log.info("Validando quantidade de baus");
+        boolean resut = chaveService.repository().findByRarity(rarity).getQuantidade() >= (quantidade);
+        if (resut) {
+            log.info("Possui " + quantidade + " baus do tipo " + rarity);
+        } else {
+            log.info("Não possui " + quantidade + " baus do tipo " + rarity);
+        }
+        return resut;
+    }
+
+    private boolean validar(Rarity rarity, Integer quantidade) {
+        return validaQuantidadeChaves(rarity, quantidade) && validaQuantidadeBaus(rarity, quantidade);
+    }
+
+    public void abrirBau(Rarity rarity, Integer quantidade) {
+        log.info("Abrindo bau do tipo " + rarity + " com " + quantidade + " chaves");
+        if (validar(rarity, quantidade)) {
+
+            chaveService.decrement(rarity, quantidade);
+            bauService.decrement(rarity, quantidade);
+
+            // Todo: Criar os drops e atribuir aqui
+
+        } else {
+            log.info("Não é possível abrir " + quantidade + " baus do tipo " + rarity);
+        }
+    }
+}
