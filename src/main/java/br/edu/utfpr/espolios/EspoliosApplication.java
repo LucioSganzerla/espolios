ea85c429cf1bf8e4b6f409d52b9b137da2cff5e6
LucioSganzerla <sganzerla.lucio1@gmail.com>
2022-05-20 20:22:04 -0300

    Testes

--- src/main/java/br/edu/utfpr/espolios/EspoliosApplication.java
+++ src/main/java/br/edu/utfpr/espolios/EspoliosApplication.java
@@ -1,10 +1,14 @@
 package br.edu.utfpr.espolios;
 
+import br.edu.utfpr.espolios.models.Bau;
 import br.edu.utfpr.espolios.models.FragmentoChave;
 import br.edu.utfpr.espolios.models.enums.Rarity;
+import br.edu.utfpr.espolios.service.AbrirBauService;
+import br.edu.utfpr.espolios.service.CRUD.BauService;
 import br.edu.utfpr.espolios.service.CRUD.ChaveService;
 import br.edu.utfpr.espolios.service.CRUD.FragmentoChaveService;
 import br.edu.utfpr.espolios.service.ForjarChaveService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +16,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@Slf4j
 public class EspoliosApplication {
 
     @Autowired
@@ -23,57 +28,84 @@ public class EspoliosApplication {
     @Autowired
     private ForjarChaveService forjarChaveService;
 
+    @Autowired
+    private BauService bauService;
+
+    @Autowired
+    private AbrirBauService abrirBauService;
+
     public static void main(String[] args) {
         SpringApplication.run(EspoliosApplication.class, args);
     }
 
     @EventListener(ApplicationReadyEvent.class)
     public void doSomethingAfterStartup() {
-        System.out.println("Adicionando fragmentos de chave...");
+        log.info("Adicionando fragmentos de chave...");
         fragmentoChaveService.save(new FragmentoChave(12, Rarity.COMMON));
         fragmentoChaveService.save(new FragmentoChave(9, Rarity.RARE));
         fragmentoChaveService.save(new FragmentoChave(6, Rarity.EPIC));
         fragmentoChaveService.save(new FragmentoChave(3, Rarity.LEGENDARY));
 
-        System.out.println("Listando fragmentos de chave...");
-        fragmentoChaveService.logKeys();
+        log.info("Listando fragmentos de chave...");
+        fragmentoChaveService.log();
 
-        System.out.println("Adicionando mais 3 fragmentos de chave as chaves comum...");
+        log.info("Adicionando mais 3 fragmentos de chave as chaves comum...");
         fragmentoChaveService.increment(Rarity.COMMON, 3);
 
-        System.out.println("Listando fragmentos de chave...");
-        fragmentoChaveService.logKeys();
+        log.info("Listando fragmentos de chave...");
+        fragmentoChaveService.log();
 
-        System.out.println("Removendo os 3 fragmentos de chave comum...");
+        log.info("Removendo os 3 fragmentos de chave comum...");
         fragmentoChaveService.decrement(Rarity.COMMON, 3);
 
-        System.out.println("Listando fragmentos de chave...");
-        fragmentoChaveService.logKeys();
+        log.info("Listando fragmentos de chave...");
+        fragmentoChaveService.log();
 
-        System.out.println("Forjando chaves do tipo comum...");
+        log.info("Forjando chaves do tipo comum...");
         forjarChaveService.forjarChave(Rarity.COMMON, 1);
 
-        System.out.println("Listando chaves...");
-        chaveService.logKeys();
+        log.info("Listando chaves...");
+        chaveService.log();
 
-        System.out.println("Forjando chaves do tipo raro...");
+        log.info("Forjando chaves do tipo raro...");
         forjarChaveService.forjarChave(Rarity.RARE, 1);
 
-        System.out.println("Listando chaves...");
-        chaveService.logKeys();
+        log.info("Listando chaves...");
+        chaveService.log();
 
-        System.out.println("Forjando todas as chaves...");
+        log.info("Forjando todas as chaves...");
         forjarChaveService.forjarTodasChaves(Rarity.COMMON);
         forjarChaveService.forjarTodasChaves(Rarity.RARE);
         forjarChaveService.forjarTodasChaves(Rarity.EPIC);
         forjarChaveService.forjarTodasChaves(Rarity.LEGENDARY);
 
-        System.out.println("Listando chaves...");
-        chaveService.logKeys();
+        log.info("Listando chaves...");
+        chaveService.log();
 
-        System.out.println("Listando fragmentos de chave...");
-        fragmentoChaveService.logKeys();
+        log.info("Listando fragmentos de chave...");
+        fragmentoChaveService.log();
 
+        log.info("Gerando 5 baus de cada para o usuário...");
+        bauService.save(new Bau(5, Rarity.COMMON));
+        bauService.save(new Bau(5, Rarity.RARE));
+        bauService.save(new Bau(5, Rarity.EPIC));
+        bauService.save(new Bau(5, Rarity.LEGENDARY));
+
+        log.info("Listando baus...");
+        bauService.log();
+
+        log.info("Abrindo baus...");
+        abrirBauService.abrirBau(Rarity.COMMON, 10);
+        abrirBauService.abrirBau(Rarity.COMMON, 2);
+        abrirBauService.abrirBau(Rarity.RARE, 3);
+        abrirBauService.abrirBau(Rarity.EPIC, 2);
+        abrirBauService.abrirBau(Rarity.LEGENDARY, 1);
+
+        log.info("Listando baus...");
+        bauService.log();
+
+        log.info("Listando chaves...");
+        chaveService.log();
 
     }
 
