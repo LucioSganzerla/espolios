c876974a388d769ce2af68811509f3697b916729
LucioSganzerla <sganzerla.lucio1@gmail.com>
2022-06-01 22:14:56 -0300

    Ajuste para utilizar o BaseRepository

--- src/main/java/br/edu/utfpr/espolios/repository/LootRepository.java
+++ src/main/java/br/edu/utfpr/espolios/repository/LootRepository.java
@@ -1,16 +1,10 @@
 package br.edu.utfpr.espolios.repository;
 
-import br.edu.utfpr.espolios.generics.repository.EntityRepository;
-import br.edu.utfpr.espolios.models.Bau;
+import br.edu.utfpr.espolios.generics.repository.BaseRepository;
 import br.edu.utfpr.espolios.models.Loot;
-import br.edu.utfpr.espolios.models.enums.Rarity;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface LootRepository extends EntityRepository<Loot> {
-
-    List<Loot> findAllByRarityAndIsDropable(Rarity rarity, boolean isDropable);
+public interface LootRepository extends BaseRepository<Loot> {
 
 }
