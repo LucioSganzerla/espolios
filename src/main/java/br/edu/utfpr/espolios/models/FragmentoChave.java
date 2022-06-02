9ecddd5da05f27cdd273e0a5053a4cbc4b00ace8
LucioSganzerla <sganzerla.lucio1@gmail.com>
2022-06-01 22:15:42 -0300

    Adição do nome da table no FragmentoChave

--- src/main/java/br/edu/utfpr/espolios/models/FragmentoChave.java
+++ src/main/java/br/edu/utfpr/espolios/models/FragmentoChave.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 @Entity
 @Getter
 @NoArgsConstructor
-@Table
+@Table(name = "fragmento_chave")
 public class FragmentoChave extends EntityModel {
 
     public FragmentoChave(Integer quantidade, Rarity rarity) {
