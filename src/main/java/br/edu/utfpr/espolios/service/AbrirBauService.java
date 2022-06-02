b02e937c534598f585c47cc6b94f650189cd10c8
LucioSganzerla <sganzerla.lucio1@gmail.com>
2022-06-01 22:17:43 -0300

    Possibilidade de abrir baus e resolução do "Todo: Criar os drops e atribuir aqui"

--- src/main/java/br/edu/utfpr/espolios/service/AbrirBauService.java
+++ src/main/java/br/edu/utfpr/espolios/service/AbrirBauService.java
@@ -1,15 +1,20 @@
 package br.edu.utfpr.espolios.service;
 
+import br.edu.utfpr.espolios.models.Inventario;
+import br.edu.utfpr.espolios.models.Loot;
 import br.edu.utfpr.espolios.models.enums.Rarity;
 import br.edu.utfpr.espolios.service.CRUD.BauService;
 import br.edu.utfpr.espolios.service.CRUD.ChaveService;
+import br.edu.utfpr.espolios.service.CRUD.InventarioService;
+import br.edu.utfpr.espolios.utils.MathUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public record AbrirBauService(BauService bauService,
-                              ChaveService chaveService) {
+                              ChaveService chaveService,
+                              InventarioService inventarioService) {
 
     private boolean validaQuantidadeChaves(Rarity rarity, Integer quantidade) {
         log.info("Validando quantidade de chaves, para verificar se é possivel abrir o bau");
@@ -37,17 +42,44 @@ public record AbrirBauService(BauService bauService,
         return validaQuantidadeChaves(rarity, quantidade) && validaQuantidadeBaus(rarity, quantidade);
     }
 
-    public void abrirBau(Rarity rarity, Integer quantidade) {
+    public void abrirPossiveis(Inventario inventario, Rarity rarity) {
+        for (int i = 0; i < chaveService.getRepository().findByRarity(rarity).getQuantidade(); i++) {
+            abrirBau(inventario, rarity, 1);
+        }
+    }
+
+    public void abrirBau(Inventario inventario, Rarity rarity, Integer quantidade) {
         log.info("Abrindo bau do tipo " + rarity + " com " + quantidade + " chaves");
         if (validar(rarity, quantidade)) {
 
             chaveService.decrement(rarity, quantidade);
             bauService.decrement(rarity, quantidade);
 
-            // Todo: Criar os drops e atribuir aqui
+            switch (rarity) {
+                case COMMON:
+                    log.info("Abrindo bau comum");
+                    inventarioService.addLoot(inventario, new Loot(Rarity.COMMON, MathUtils.random(0, 10)));
+                    break;
+                case RARE:
+                    log.info("Abrindo bau raro");
+                    inventarioService.addLoot(inventario, new Loot(Rarity.RARE, MathUtils.random(10.01, 20)));
+                    break;
+                case EPIC:
+                    log.info("Abrindo bau epico");
+                    inventarioService.addLoot(inventario, new Loot(Rarity.EPIC, MathUtils.random(20.01, 30)));
+                    break;
+                case LEGENDARY:
+                    log.info("Abrindo bau lendário");
+                    inventarioService.addLoot(inventario, new Loot(Rarity.LEGENDARY, MathUtils.random(30.01, 200)));
+                    break;
+            }
+
+            log.info("Bau aberto com sucesso");
 
         } else {
             log.info("Não é possível abrir " + quantidade + " baus do tipo " + rarity);
         }
     }
+
+
 }
