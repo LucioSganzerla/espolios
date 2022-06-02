bc0d7ffaf8345716c4da6cdd4c4fdeb70c2fa7be
LucioSganzerla <sganzerla.lucio1@gmail.com>
2022-06-01 22:15:20 -0300

    Remoção dos métodos duplicados (Os quais existem no BaseRepository)

--- src/main/java/br/edu/utfpr/espolios/service/CRUD/LootService.java
+++ src/main/java/br/edu/utfpr/espolios/service/CRUD/LootService.java
@@ -1,8 +1,5 @@
 package br.edu.utfpr.espolios.service.CRUD;
 
-import br.edu.utfpr.espolios.models.FragmentoChave;
-import br.edu.utfpr.espolios.models.Loot;
-import br.edu.utfpr.espolios.models.enums.Rarity;
 import br.edu.utfpr.espolios.repository.LootRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,23 +7,5 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public record LootService(LootRepository repository) {
-
-    public void save(Loot loot) {
-        log.info("Salvando loot {}", loot.getRarity());
-        repository.saveAndFlush(loot);
-        log.info("Loot Salvo Tipo {}", loot.getRarity());
-    }
-
-    public void logAdquiridos(Rarity rarity) {
-        log.info("Todos os loots adquiridos da rariedade {}", rarity);
-        repository.findAllByRarityAndIsDropable(rarity, false).forEach(loot -> log.info("{}", loot));
-        log.info("Fim dos loots");
-    }
-
-    public void logDisponiveis(Rarity rarity) {
-        log.info("Todos os loots disponiveis da raridade {}", rarity);
-        repository.findAllByRarityAndIsDropable(rarity, true).forEach(loot -> log.info("{}", loot));
-        log.info("Fim dos loots");
-    }
 
 }
