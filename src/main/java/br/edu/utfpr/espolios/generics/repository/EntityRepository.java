8c2366f9ff47f8c75e91202bf8b2a34514b9b6c1
LucioSganzerla <sganzerla.lucio1@gmail.com>
2022-06-01 22:16:49 -0300

    EntityRepository.java

--- src/main/java/br/edu/utfpr/espolios/generics/repository/EntityRepository.java
+++ src/main/java/br/edu/utfpr/espolios/generics/repository/EntityRepository.java
@@ -1,16 +1,12 @@
 package br.edu.utfpr.espolios.generics.repository;
 
+import br.edu.utfpr.espolios.generics.model.EntityModel;
 import br.edu.utfpr.espolios.models.enums.Rarity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
-import java.util.List;
-
 @NoRepositoryBean
-public interface EntityRepository<T> extends JpaRepository<T, Long> {
+public interface EntityRepository<T extends EntityModel> extends BaseRepository<T> {
 
     T findByRarity(Rarity rarity);
-
-    List<T> findAllByRarity(Rarity rarity);
 
 }
