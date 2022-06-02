f967fcbb4b0451fbad32c75cb713a53335970435
LucioSganzerla <sganzerla.lucio1@gmail.com>
2022-06-01 22:13:09 -0300

    Remoção import sem uso - BaseService.java

--- src/main/java/br/edu/utfpr/espolios/generics/BaseService.java
+++ src/main/java/br/edu/utfpr/espolios/generics/BaseService.java
@@ -2,7 +2,6 @@ package br.edu.utfpr.espolios.generics;
 
 import br.edu.utfpr.espolios.generics.model.BaseModel;
 import br.edu.utfpr.espolios.generics.repository.BaseRepository;
-import br.edu.utfpr.espolios.generics.repository.EntityRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
