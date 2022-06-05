755e6f610e0499471f1fb47a90515734ffab1b06
LucioSganzerla <sganzerla.lucio1@gmail.com>
2022-06-04 23:02:15 -0300

    Refatoração da classe LootService.java para ficar no padrão dos demais Services

--- src/main/java/br/edu/utfpr/espolios/service/CRUD/LootService.java
+++ src/main/java/br/edu/utfpr/espolios/service/CRUD/LootService.java
@@ -1,11 +1,22 @@
 package br.edu.utfpr.espolios.service.CRUD;
 
+import br.edu.utfpr.espolios.generics.BaseService;
+import br.edu.utfpr.espolios.models.Loot;
 import br.edu.utfpr.espolios.repository.LootRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public record LootService(LootRepository repository) {
+@RequiredArgsConstructor
+public class LootService extends BaseService<Loot> {
+
+    private final LootRepository repository;
+
+    @Override
+    public LootRepository getRepository() {
+        return repository;
+    }
 
 }
