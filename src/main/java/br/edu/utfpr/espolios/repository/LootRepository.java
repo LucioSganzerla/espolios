38fa3493e2b81d257ce4b8938920c11f2f87a7ad
LucioSganzerla <sganzerla.lucio1@gmail.com>
2022-06-04 23:02:43 -0300

    findAllByInventarioId(Long id) no LootRepository.java

--- src/main/java/br/edu/utfpr/espolios/repository/LootRepository.java
+++ src/main/java/br/edu/utfpr/espolios/repository/LootRepository.java
@@ -4,7 +4,11 @@ import br.edu.utfpr.espolios.generics.repository.BaseRepository;
 import br.edu.utfpr.espolios.models.Loot;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LootRepository extends BaseRepository<Loot> {
+
+    List<Loot> findAllByInventarioId(Long id);
 
 }
